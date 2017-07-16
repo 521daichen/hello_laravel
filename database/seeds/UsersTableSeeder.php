3427d9ef509f98b30a867161897f7cb7309a59f8
daichen <543577508@qq.com>
2017-07-16 23:56:25 +0800

    Finish user edit, update, index, and destroy actions

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Aufree';
         $user->email = 'aufree@estgroupe.com';
+        $user->is_admin = true;
         $user->password = bcrypt('password');
         $user->save();
     }
